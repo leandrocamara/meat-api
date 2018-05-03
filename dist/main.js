edbc0af61ca5c166cb7e1fd021afb44c5cd82bdc
Leandro Câmara <leandrocamara_eng@outlook.com>
2018-05-03 00:03:48 -0300

    ADD - Primeio exemplo com Restify.

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,15 @@
-console.log('main script...');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+server.get('/hello', (req, resp, next) => {
+    resp.json({ message: 'Hello World!' });
+    return next();
+});
+server.listen(3000, () => {
+    console.log('API is running on http://localhost:3000');
+});
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,2 +1,16 @@
 
-console.log('main script...')
+import * as restify from 'restify'
+
+const server = restify.createServer({
+  name: 'meat-api',
+  version: '1.0.0'
+})
+
+server.get('/hello', (req, resp, next) => {
+  resp.json({ message: 'Hello World!' })
+  return next()
+})
+
+server.listen(3000, () => {
+  console.log('API is running on http://localhost:3000')
+})
