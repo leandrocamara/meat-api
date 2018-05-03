dde5713f8bdd0155856259d22c73184bed650eb1
Leandro da Silva Câmara <leandrocamara_eng@outlook.com>
2018-05-03 13:14:44 -0300

    ADD - Exemplos de uso dos objetos 'Request', 'Response' e 'Next()'.

--- main.ts
+++ main.ts
@@ -6,10 +6,31 @@ const server = restify.createServer({
   version: '1.0.0'
 })
 
-server.get('/hello', (req, resp, next) => {
-  resp.json({ message: 'Hello World!' })
+server.use(restify.plugins.queryParser())
+
+server.get('/info', [
+  (req, resp, next) => {
+    if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+      let error: any = new Error();
+      error.statusCode = 400
+      error.message = 'Please, update your browser'
+      return next(error)
+    }
+    return next()
+},(req, resp, next) => {
+  // resp.contentType = 'application/json'
+  // resp.status(400)
+  // resp.setHeader('Content-Type', 'application/json')
+  // resp.send({ message: 'hello' })
+  resp.json({
+    browser: req.userAgent(),
+    method: req.method,
+    url: req.href(),
+    path: req.path(),
+    query: req.query
+  })
   return next()
-})
+}])
 
 server.listen(3000, () => {
   console.log('API is running on http://localhost:3000')
