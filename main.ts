41b71099885c52c8282b99733f79e417b3f20eda
Leandro da Silva Câmara <leandrocamara_eng@outlook.com>
2018-05-03 16:37:21 -0300

    UPDATE - Criação da classe do Servidor.

--- main.ts
+++ main.ts
@@ -1,37 +1,11 @@
 
-import * as restify from 'restify'
+import {Server} from './server/server'
 
-const server = restify.createServer({
-  name: 'meat-api',
-  version: '1.0.0'
-})
-
-server.use(restify.plugins.queryParser())
-
-server.get('/info', [
-  (req, resp, next) => {
-    if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-      let error: any = new Error();
-      error.statusCode = 400
-      error.message = 'Please, update your browser'
-      return next(error)
-    }
-    return next()
-},(req, resp, next) => {
-  // resp.contentType = 'application/json'
-  // resp.status(400)
-  // resp.setHeader('Content-Type', 'application/json')
-  // resp.send({ message: 'hello' })
-  resp.json({
-    browser: req.userAgent(),
-    method: req.method,
-    url: req.href(),
-    path: req.path(),
-    query: req.query
-  })
-  return next()
-}])
-
-server.listen(3000, () => {
-  console.log('API is running on http://localhost:3000')
+const server = new Server()
+server.bootstrap().then(server => {
+  console.log('Server is listening on: ', server.application.address())
+}).catch(error => {
+  console.log('Server failed to start')
+  console.error(error)
+  process.exit(1)
 })
