f386edf63ec4a3db5eab0c257a2ac4cc1e644a8c
Leandro Câmara <leandrocamara_eng@outlook.com>
2018-05-08 23:54:21 -0300

    ADD - Adicionado validadores ao model 'User'.

--- dist/users/users.model.js
+++ dist/users/users.model.js
@@ -5,9 +5,28 @@ const mongoose = require("mongoose");
  * Schema: Define as propriedades (metadados) dos documentos.
  */
 const userSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, unique: true },
-    password: { type: String, select: false }
+    name: {
+        type: String,
+        required: true,
+        maxlength: 80,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        unique: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        required: true
+    },
+    password: {
+        type: String,
+        select: false,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: false,
+        enum: ['Male', 'Female']
+    }
 });
 /**
  * Exporta o Model "User", que faz uso do Schema "userSchema".
